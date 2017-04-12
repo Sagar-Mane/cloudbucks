b5c4c27ff9f22664e6c88691adb92b93c0e273a5
Sagar Mane <sagar.mane006@gmail.com>
2017-04-12 00:09:34 -0700

    Seperation of Database Connection

--- src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/Starbucks_Api.py
+++ src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/Starbucks_Api.py
@@ -1,23 +1,18 @@
 
 
 from flask import request
+import Mongo_Connection
 
-
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client.starbucks   #database
-collection = db.orders  #collection
 
 def ping():
     return "success"
 
 def place_Order(x):
     data = request.get_json(force=True)
-    collection.insert_one({"id": x, "order": data})
+    Mongo_Connection.collection.insert_one({"id": x, "order": data})
     return data
 
 def get_Order(order_id):
-    data = collection.find_one({"id": order_id}, {"_id": 0})
+    data = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
     return data
 
