00702148560ceb48d550ac4513721044619e70f6
Sagar Mane <sagar.mane006@gmail.com>
2017-04-15 09:29:26 -0700

    Adding store 2 and store 3 pages

--- Starbucks_Drink_Ordering_Portal/public/Angular/main.js
+++ Starbucks_Drink_Ordering_Portal/public/Angular/main.js
@@ -5,8 +5,23 @@ app.config(function($routeProvider) {
 	.when("/store1", {
 		templateUrl : "Templates/store1.html",
 		controller : "store1_controller"
-	})
+	}).when("/store2", {
+		templateUrl : "Templates/store2.html",
+		controller : "store2_controller"
+	}).when("/store3", {
+		templateUrl : "Templates/store3.html",
+		controller : "store3_controller"
+	});
 });
-app.controller("store1_controller",function($scope,$http){
+app.controller("store1_controller",function($scope,$http,$rootScope){
+	$rootScope.bgimg = "https://s-media-cache-ak0.pinimg.com/originals/1b/60/78/1b6078385f4181319017a1b6859fc31c.jpg";
 	console.log("Reporting from store 1 controller");
+});
+
+app.controller("store2_controller",function($scope,$http){
+	console.log("Reporting from store 2 controller");
+});
+
+app.controller("store3_controller",function($scope,$http){
+	console.log("Reporting from store 3 controller");
 });
