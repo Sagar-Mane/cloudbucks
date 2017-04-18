093220173fccd3c090a237ba76a09f03a1c535c1
Sagar Mane <sagar.mane006@gmail.com>
2017-04-17 19:38:56 -0700

    Refactoring 1. Bug Fixes in API's 2. Order processing logic corrected 3. JSON reponses standardized

--- src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/Starbucks_Api.py
+++ src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/Starbucks_Api.py
@@ -1,45 +1,86 @@
-from json import dumps
-
 from flask import request, jsonify, json
 import Mongo_Connection
+import time
+from threading import Thread
 
 
 def ping():
     return "success"
 
-def place_Order(x):
+def place_Order(order_id):
     data = request.get_json(force=True)
-    data['id']=x
-    print data
-    Mongo_Connection.collection.insert_one({"order":data})
-    return data
+    links={
+        "payment": "localhost:9090/v3/starbucks/order/" + order_id + "/pay",
+        "order":"localhost:9090/v3/starbucks/order/" + order_id
+    }
+    data['links']=links
+    data['id']=order_id
+    data['status']="PLACED"
+    data['message']="Order has been placed"
+    Mongo_Connection.collection.insert_one(data)
+    order= Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+    return order
 
 def get_Order(order_id):
-    data = Mongo_Connection.collection.find_one({"order.id": order_id}, {"_id": 0})
-    return data
+    order = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+    return json.dumps(order)
 
 def get_Orders():
-    data=[]
+    orders=[]
     cursor = Mongo_Connection.collection.find(projection={'_id':False})
     for document in cursor:
-        data.append(document)
-    print "Printing array data",data
+        orders.append(document)
 
-    return jsonify({'orders':data})
+    return json.dumps(orders)
 
 def cancel_Order(order_id):
-    print "Cancelling order with id",order_id
-    Mongo_Connection.collection.remove({"id":order_id})
-    return "Order Cancelled"
+    order = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+    print order
+    if order is None:
+        message={
+            'status':"error",
+            'message':"Order not found"
+        }
+        return json.dumps(message)
+    else:
+        Mongo_Connection.collection.remove({"id": order_id})
+        message = {
+            'status': "success",
+            'message': "Order cancelled"
+        }
+        return json.dumps(message)
 
 def update_Order(order_id):
-    print "Updating order with id",order_id
     data = request.get_json(force=True)
-    print data
-    Mongo_Connection.collection.update({'id':order_id}, {"$set": data}, upsert=False)    #updating the order
-    return "Order Updated"
+    Mongo_Connection.collection.update({"id": order_id},{"$set":{"items":data['items']}})   #updating the order
+    order = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+    return json.dumps(order)
 
 def pay_Order(order_id):
-    print "Paying for the order with id",order_id
-    Mongo_Connection.collection.update({'order.id': order_id}, {"$set": {'order.status': "Paid"}})
-    return "Order Status Paid"
+    order = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+    if order['status']=="PAID" or order['status']=="PREPARING" or order['status']=="SERVED" or order['status']=="COLLECTED":
+        message={
+            'status':"error",
+            'message':"Order payment rejected"
+        }
+        return json.dumps(message)
+    else:
+        Mongo_Connection.collection.update({'id': order_id}, {"$set": {'status': "PAID", 'message': "Payment Accepted"}})
+        Mongo_Connection.collection.update({'id': order_id}, {"$unset": {'links.payment': ''}})
+        worker(order_id).start()
+        order = Mongo_Connection.collection.find_one({"id": order_id}, {"_id": 0})
+        return json.dumps(order)
+
+class worker(Thread):
+    def __init__(self,order_id):
+        super(worker,self).__init__()
+        self.order_id=order_id
+    def run(self):
+        time.sleep(5)
+        Mongo_Connection.collection.update({'id': self.order_id}, {"$set": {'status': "PREPARING",'message':"Order preparations in progress"}})
+        time.sleep(2)
+        Mongo_Connection.collection.update({'id': self.order_id}, {"$set": {'status': "SERVED",'message':"Order served, waiting for customer pick up"}})
+        time.sleep(10)
+        Mongo_Connection.collection.update({'id': self.order_id}, {"$set": {'status': "COLLECTED",'message':"Order retrived by customer"}})
+
+
--- src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/app.py
+++ src/tenant/tenant_python_mongo/Starbucks-Multi_Cloud_Backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import request, jsonify
+from flask import Flask, json
+from flask import jsonify
 import uuid
 import Starbucks_Api
 
@@ -10,39 +10,38 @@ def main():
     print "Ping Api"
     return jsonify({'Ping': Starbucks_Api.ping()})
 
-@app.route("/starbucks/order", methods=['POST'])
+@app.route("/v3/starbucks/order", methods=['POST'])
 def place_order():
     order_id = uuid.uuid1()
-    x = str(order_id)
-    data=Starbucks_Api.place_Order(x)
-    return jsonify({'order': data})
+    id = str(order_id)
+    order=Starbucks_Api.place_Order(id)
+    return json.dumps(order)
 
-@app.route("/starbucks/order/<string:order_id>", methods=['GET'])
+@app.route("/v3/starbucks/order/<string:order_id>", methods=['GET'])
 def get_order(order_id):
-    data=Starbucks_Api.get_Order(order_id)
-    return jsonify({'order':data})
+    order=Starbucks_Api.get_Order(order_id)
+    return order
 
-@app.route("/starbucks/orders", methods=['GET'])
+@app.route("/v3/starbucks/orders", methods=['GET'])
 def get_orders():
-    data=Starbucks_Api.get_Orders()
-    return data
+    orders=Starbucks_Api.get_Orders()
+    return orders
 
-@app.route("/starbucks/order/<string:order_id>", methods=['DELETE'])
+@app.route("/v3/starbucks/order/<string:order_id>", methods=['DELETE'])
 def cancel_order(order_id):
-    print order_id
     status=Starbucks_Api.cancel_Order(order_id)
-    return jsonify({'status': status})
+    return status
 
-@app.route("/starbucks/order/<string:order_id>", methods=['PUT'])
+@app.route("/v3/starbucks/order/<string:order_id>", methods=['PUT'])
 def update_order(order_id):
-    message=Starbucks_Api.update_Order(order_id)
-    return jsonify({'message': message})
+    order=Starbucks_Api.update_Order(order_id)
+    return order
 
-@app.route("/starbucks/order/<string:order_id>/pay", methods=['POST'])
+@app.route("/v3/starbucks/order/<string:order_id>/pay", methods=['POST'])
 def pay_order(order_id):
-    message=Starbucks_Api.pay_Order(order_id)
-    return jsonify({"message":message})
+    order=Starbucks_Api.pay_Order(order_id)
+    return order
 
 if __name__ == "__main__":
-    print "Python Server Running at port 5000"
-    app.run()
+    print "Python Server Running at port 90"
+    app.run(port=90)
