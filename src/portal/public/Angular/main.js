d0ad8c986afefbad9fc4590cf48c440cc8f5ae0f
Abhishek Madan <abhishekmadan60@gmail.com>
2017-04-21 06:07:41 -0700

    payment request

--- src/portal/public/Angular/main.js
+++ src/portal/public/Angular/main.js
@@ -1,4 +1,6 @@
 var app = angular.module('myapp', [ 'ngRoute' ]);
+var link = 'http://52.53.220.34:8000';
+
 app.config(function($routeProvider) {
 	console.log("in route provider");
 	$routeProvider.when("/store1", {
@@ -14,80 +16,82 @@ app.config(function($routeProvider) {
 
 });
 
-app
-		.controller(
-				"store1_controller",
-				function($scope, $http, $rootScope, $interval) {
-					$scope.success = true;
-					console.log("Reporting from store 1 controller");
-					$scope.show = false;
-					$scope.getOrders = function() {
-						$http(
-								{
-									method : 'GET',
-									url : 'http://52.53.220.34:8000/store1/v3/starbucks/orders',
-								}).success(function(data) {
-							console.log("Order Paid" + JSON.stringify(data));
-							$scope.orders = data;
-						});
-					};
+app.controller("store1_controller", function($scope, $http, $route, $rootScope,
+		$interval) {
+	$scope.success = true;
+	console.log("Reporting from store 1 controller");
+	$scope.show = false;
+	
+	$scope.getOrders = function() {
+		$http({
+			method : 'GET',
+			url : link + '/store1/v3/starbucks/orders',
+		}).success(function(data) {
+			console.log("Order Paid" + JSON.stringify(data));
+			$scope.orders = data;
+		});
+	};
+	
+	$scope.placeOrder = function() {
+		console.log("Reporting from place order");
+		console.log("Order:\n Name:" + $scope.name + "\nSize:" + $scope.size
+				+ "\nMilk:" + $scope.milk + "\nQuantity:" + $scope.quantity);
+		$http({
+			method : 'POST',
+			url : link + 'store1/v3/starbucks/order',
+			data : {
 
-					$scope.getOrders();
+			}
+		}).success(function(data) {
+			console.log("Order PLACED");
+		});
 
-					$scope.placeOrder = function() {
-						console.log("Reporting from place order");
-						console.log("Order:\n Name:" + $scope.name + "\nSize:"
-								+ $scope.size + "\nMilk:" + $scope.milk
-								+ "\nQuantity:" + $scope.quantity);
-						$http(
-								{
-									method : 'POST',
-									url : 'http://54.183.220.137:90/v3/starbucks/order',
-									data : {
+	};
+	
+	$scope.deleteOrder = function(order_id) {
+		console.log("Reporting from delete order" + order_id);
+		$http({
+			method : 'DELETE',
+			url : link + 'store1/v3/starbucks/order/' + order_id,
+		}).success(function(data) {
+			console.log("Order DELETED");
+		});
 
-									}
-								}).success(function(data) {
-							console.log("Order PLACED");
-						});
+	};
+	
+	$scope.updateOrder = function(order_id) {
+		console.log("Reporting from update order" + order_id);
+		$http({
+			method : 'PUT',
+			url : link + 'store1/v3/starbucks/order/' + order_id,
+			data : {
 
-					};
-					$scope.deleteOrder = function(order_id) {
-						console.log("Reporting from delete order" + order_id);
-						$http(
-								{
-									method : 'DELETE',
-									url : 'http://54.183.220.137:90/v3/starbucks/order/<string:order_id>',
-								}).success(function(data) {
-							console.log("Order DELETED");
-						});
+			}
+		}).success(function(data) {
+			console.log("Order UPDATED");
+		});
 
-					};
-					$scope.updateOrder = function(order_id) {
-						console.log("Reporting from update order" + order_id);
-						$http(
-								{
-									method : 'PUT',
-									url : 'http://54.183.220.137:90/v3/starbucks/order/<string:order_id>',
-									data : {
+	};
+	
+	$scope.payOrder = function(order_id) {
+		console.log("Reporting from pay order" + order_id);
+		$http({
+			method : 'POST',
+			url : link + 'store1/v3/starbucks/order/' + order_id + '/pay',
+		}).success(function(data) {
+			console.log(data);
+			console.log("Order PAID");
+			setTimeout($route.reload(), 3000);
+		});
+	};
+	
+	$scope.getOrders();
+});
 
-									}
-								}).success(function(data) {
-							console.log("Order UPDATED");
-						});
 
-					};
-					$scope.payOrder = function(order_id) {
-						console.log("Reporting from pay order" + order_id);
-						$http(
-								{
-									method : 'POST',
-									url : 'http://54.183.220.137:90/v3/starbucks/order/<string:order_id>',
-								}).success(function(data) {
-							console.log("Order PAID");
-						});
-					};
-				});
-
+/**
+ * Store 2
+ */
 app.controller("store2_controller", function($scope, $route, $http) {
 	console.log("Reporting from store 2 controller");
 	$scope.success = true;
@@ -96,28 +100,32 @@ app.controller("store2_controller", function($scope, $route, $http) {
 	$scope.getOrders = function() {
 		$http({
 			method : 'GET',
-			url : 'http://52.53.220.34:8000/store2/v3/starbucks/orders',
+			url : link + '/store2/v3/starbucks/orders',
 		}).success(function(data) {
 			console.log("Order Paid" + JSON.stringify(data));
 			$scope.orders = data;
 		});
 	};
+	
 	$scope.payOrder = function(order_id) {
 		console.log("Reporting from pay order" + order_id);
-		$http(
-				{
-					method : 'POST',
-					url : 'http://52.53.220.34:8000/store2/v3/starbucks/order/'+order_id+'/pay',
-				}).success(function(data) {
-					console.log(data);
-					console.log("Order PAID");
-					setTimeout($route.reload(),3000);
+		$http({
+			method : 'POST',
+			url : link + '/store2/v3/starbucks/order/' + order_id + '/pay',
+		}).success(function(data) {
+			console.log(data);
+			console.log("Order PAID");
+			setTimeout($route.reload(), 3000);
 		});
 	};
+	
 	$scope.getOrders();
 });
 
-app.controller("store3_controller", function($scope, $http) {
+/**
+ * Store 3
+ */
+app.controller("store3_controller", function($scope, $route, $http) {
 	console.log("Reporting from store 3 controller");
 
 	$scope.success = true;
@@ -126,10 +134,22 @@ app.controller("store3_controller", function($scope, $http) {
 	$scope.getOrders = function() {
 		$http({
 			method : 'GET',
-			url : 'http://52.53.220.34:8000/store2/v3/starbucks/orders',
+			url : link + '/store3/v3/starbucks/orders',
 		}).success(function(data) {
 			console.log("Order Paid" + JSON.stringify(data));
 			$scope.orders = data;
+		});
+	};
+
+	$scope.payOrder = function(order_id) {
+		console.log("Reporting from pay order" + order_id);
+		$http({
+			method : 'POST',
+			url : link + '/store3/v3/starbucks/order/' + order_id + '/pay',
+		}).success(function(data) {
+			console.log(data);
+			console.log("Order PAID");
+			setTimeout($route.reload(), 3000);
 		});
 	};
 
