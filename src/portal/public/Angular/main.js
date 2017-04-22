23ff0d840e1590da5682cbe189ad19e36fcd6faf
sjsu-sushant <sushantvairagade@outlook.com>
2017-04-22 15:49:48 -0700

    Adding below changes: -Removed redundant changes -Made few UI changes

--- src/portal/public/Angular/main.js
+++ src/portal/public/Angular/main.js
@@ -1,17 +1,17 @@
 var app = angular.module('myapp', [ 'ngRoute' ]);
 
-var link = 'http://localhost:9090';
+var link = 'http://192.168.99.100:90';
 
 app.config(function($routeProvider) {
 	console.log("in route provider");
 	$routeProvider.when("/store1", {
-		templateUrl : "Templates/store1.html",
+		templateUrl : "Templates/store.html",
 		controller : "store1_controller"
 	}).when("/store2", {
-		templateUrl : "Templates/store2.html",
+		templateUrl : "Templates/store.html",
 		controller : "store2_controller"
 	}).when("/store3", {
-		templateUrl : "Templates/store3.html",
+		templateUrl : "Templates/store.html",
 		controller : "store3_controller"
 	});
 
--- src/portal/public/Angular/store1.js
+++ src/portal/public/Angular/store1.js
@@ -6,88 +6,151 @@
  * Store 1
  */
 
-var link = 'http://localhost:9090';
 
-angular.module('myapp').controller("store1_controller", function($scope, $http, $route, $rootScope,
-		$interval) {
-	$scope.success = true;
-	console.log("Reporting from store 1 controller nachiket");
-	$scope.show = false;
-	
-	$scope.getOrders = function() {
-		$http({
-			method : 'GET',
-			url : link + '/store1/v3/starbucks/orders',
-		}).success(function(data) {
-			console.log("Order Paid" + JSON.stringify(data));
-			$scope.orders = data;
-		});
-	};
-	
-	$scope.placeOrder = function() {
-		console.log("Reporting from place order");
-		
-		var OrderDetails = {
-						"location" : "store-1",
-						"items" : [ {
-						"qty" : $scope.quantity,
-						"name" : $scope.name,
-						"milk" : $scope.milk,
-						"size" : $scope.size
-						} ]
-					};
-		
-		console.log(OrderDetails);
-						
-		$http({
-			method : 'POST',
-			url : link + '/store2/v3/starbucks/order',
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			data : OrderDetails
-			
-		}).success(function(data) {
-			console.log("Order PLACED");
-		});
+angular.module('myapp').controller("store1_controller", function ($scope, $http, $route, $rootScope,
+                                                                  $interval) {
+    $scope.success = true;
+    console.log("Reporting from store 1 controller");
+    $scope.show = false;
 
-	};
+    $scope.place_order_flag = true;
+    $scope.update_order_flag = true;
+    $scope.pay_order_flag = true;
+    $scope.del_order_flag = true;
 
-	
-	$scope.deleteOrder = function(url) {
-		console.log("Reporting from delete order " + url);
-		$http({
-			method : 'DELETE',
-			url : url,
-		}).success(function(data) {
-			console.log("Order DELETED");
-		});
+    storeName = '/store1';
+    $scope.getOrders = function () {
+        $http({
+            method: 'GET',
+            url: link + storeName + '/v3/starbucks/orders',
+        }).success(function (data) {
+            console.log("Order Paid" + JSON.stringify(data));
+            $scope.orders = data;
+        });
+    };
 
-	};
-	
-	$scope.updateOrder = function(url) {
-		console.log("Reporting from update order " + url);
-		$http({
-			method : 'PUT',
-			url : url,
-			data : {
+    $scope.placeOrder = function () {
+        console.log("Reporting from place order");
 
-			}
-		}).success(function(data) {
-			console.log("Order UPDATED");
-		});
+        var order = {
+            "location": "store-1",
+            "items": [{
+                "qty": $scope.quantity,
+                "name": $scope.name,
+                "milk": $scope.milk,
+                "size": $scope.size
+            }]
+        };
 
-	};
-	
-	$scope.payOrder = function(url) {
-		console.log("Reporting from pay order " + url);
-		$http({
-			method : 'POST',
-			url : url,
-		}).success(function(data) {
-			console.log(data);
-			console.log("Order PAID");
-			setTimeout($route.reload(), 3000);
-		});
-	};
-	
-	$scope.getOrders();
+        console.log(order);
+
+        $http({
+            method: 'POST',
+            url: link + '/v3/starbucks/order',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: order
+
+        }).success(function (data) {
+            console.log("Order placed");
+            //message should be displayed that your order has been placed
+            //manage this flag in UI
+            $scope.place_order_flag = false;
+        });
+
+    };
+
+
+    $scope.deleteOrder = function (url) {
+        console.log("Reporting from delete order " + url);
+        $http({
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            method: 'DELETE',
+            url: url
+        }).success(function (data) {
+            console.log("Order Deleted");
+            $scope.flag_del = "Order cancelled"
+            //message should be displayed that your order has been DELETED
+            //manage this flag in UI
+            $scope.del_order_flag = false;
+            $route.reload();
+            console.log("function deleteOrder ended");
+        }).error(function(error, status) {
+            console.log(error);
+            $scope.flag_del = error.message;
+            $scope.del_order_flag = false;
+        });
+
+    };
+
+    $scope.updateOrder = function (url) {
+        console.log("Reporting from update order " + url);
+        var url = $scope.up_url;
+        console.log(url);
+
+        var order = {
+            "location" : "store-1",
+            "items" : [ {
+                "qty" : $scope.up_quantity,
+                "name" : $scope.up_name,
+                "milk" : $scope.up_milk,
+                "size" : $scope.up_size
+            } ]
+        };
+
+        console.log(order);
+
+        $http({
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            method: 'PUT',
+            url: url,
+            data: order
+        }).success(function (data) {
+            console.log("ORDER HAS BEEN UPDATED");
+            console.log(data.status);
+
+            //message should be displayed that your order has been placed
+            //manage this flag in UI
+            $scope.update_order_flag = false;
+        }).error(function(error, status) {
+            console.log(error);
+            $scope.flag = error.message;
+            $scope.update_order_flag = false;
+        });
+
+    };
+
+    $scope.showUpdatePopUp = function (url) {
+        console.log("inside showUpdatePopUp");
+        console.log(url);
+        $scope.up_url = url;
+        document.getElementById('id02').style.display='block';
+        //document.getElementById('url').value = url;
+    }
+
+    $scope.payOrder = function (url) {
+        console.log("Reporting from pay order " + url);
+        if(!url) {
+            $scope.flag_pay = "Can't pay for this order";
+            $scope.pay_order_flag = false;
+            return;
+        }
+
+
+        $http({
+            method: 'POST',
+            url: url,
+        }).success(function (data) {
+            console.log(data);
+            console.log("Order PAID");
+            $scope.flag_pay = "Payment done"
+            $scope.pay_order_flag = false;
+            console.log("Order PAID");
+            $route.reload();
+        }).error(function(error, status) {
+            console.log(error);
+            $scope.flag_pay = error.message;
+            $scope.pay_order_flag = false;
+        });
+    };
+    $scope.getOrders();
 });
